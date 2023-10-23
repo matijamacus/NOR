a7dda02976e64719af13863c7affdcd3ace8439a
matijamacus <146855311+matijamacus@users.noreply.github.com>
2023-10-23 17:06:45 +0200

    Update izris_grafa.m

--- izris_grafa.m
+++ izris_grafa.m
@@ -15,8 +15,5 @@ function izris_grafa(St_Tock, R)
     
     Plot_kroznice(R);
     axis equal;
-    title('Razporeditev naključno generiranih točk');
-    xlabel('X-os');
-    ylabel('Y-os');
-    legend('Znotraj krožnice', 'Zunaj krožnice', 'Krožnica');
+   
 end
